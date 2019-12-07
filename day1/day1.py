f6f5be7fcd00f44dd97278330275c54826d07666
annekeller <annekeller93@gmail.com>
2019-12-07 12:13:07 +0100

    completed day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,16 @@
 #day1
 #to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2
 import math
-mass = 12
-fuel = mass / 3
-math.floor(fuel)
-fuel = fuel - 2
-print(fuel)
 
 def sumAllFuel ():
     allFuel = ""
-    with open("day1.txt", "r") as file:
-        allFuel = file.read().replace('\n', '')
-        print(allFuel)
+    sum = 0
+    with open("/Users/annekeller/Documents/Chalmers/Miscellenous/Programmering/adventofcode2019/day1/day1.txt", "r") as file:
+        allFuel = file.readlines()
+        allFuel = list(map(int, allFuel))
+        for i in range(len(allFuel)):
+            allFuel[i] = (math.floor(allFuel[i] / 3) - 2)
+            sum = sum + allFuel[i] 
+        print(math.floor(sum))
 
-sumAllFuel()
+sumAllFuel() 
